04cab6d283e05ec0caad62381d4a722d2b7bcf21
AvKuroro <117792104+AvKuroro@users.noreply.github.com>
2026-03-29 20:24:21 -0300

    Update ITEM_1.py
    Agrege 1B

--- ITEM_1.py
+++ ITEM_1.py
@@ -29,10 +29,32 @@ def calcular_maximo(datos):
                         maximo = numeros
         return maximo
 
-
-
-
-
+# 1B Ordenamiento Bubble Sort
+def bubble_sort(datos, descendente=False):
+    lista_ordenada = datos[:]
+    cantidad = len(lista_ordenada)
+    
+    for pasada in range(cantidad - 1):
+        intercambio_hecho = False
+        
+        for posicion in range(0, cantidad - pasada - 1):
+            if descendente:
+                if lista_ordenada[posicion] < lista_ordenada[posicion + 1]:
+                    temp = lista_ordenada[posicion]
+                    lista_ordenada[posicion] = lista_ordenada[posicion + 1]
+                    lista_ordenada[posicion + 1] = temp
+                    intercambio_hecho = True
+            else:
+                if lista_ordenada[posicion] > lista_ordenada[posicion + 1]:
+                    temp = lista_ordenada[posicion]
+                    lista_ordenada[posicion] = lista_ordenada[posicion + 1]
+                    lista_ordenada[posicion + 1] = temp
+                    intercambio_hecho = True
+                    
+        if not intercambio_hecho:
+            break
+    
+    return lista_ordenada
 
 # 1E REPORTE ESTADISTICO INTEGRADO
 ciudades = [
